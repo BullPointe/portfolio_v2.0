4a4f45c830a198cc01a13e55d5990199196947e9
Sahil Jaganmohan <dev.sahil.jaganmohan@gmail.com>
2021-06-20 01:38:15 -0400

    DropDown Nav Completed

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,61 @@
-import React from "react"
+import React,{Component} from "react"
+import { Link } from "gatsby"
+import {FaBars} from "react-icons/fa"
+import {GiCrossedSabres} from "react-icons/gi"
+import "./headerstyle.css"
 
-const Header = () => {
-  return (
-    //   <nav className="flex items-center justify-between flex-nowrap bg-blue-200 p-6">
-    <div class="grid grid-cols-5 gap-4">
-        <ul class="px-4 flex mt-4 col-start-3 col-span-5">
-            <li class="flex-1 mr-2">
-                <a class="text-center block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white" href="#">Active Item</a>
-            </li>
-            <li class="flex-1 mr-2">
-                <a class="text-center block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4" href="#">Nav Item</a>
-            </li>
-           
-        </ul>
-    </div>
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+    this.closeNav = this.closeNav.bind(this);
+    this.openNav = this.openNav.bind(this);
+  }
 
-  )
+  openNav() {
+    this.myRef.current.style.height = "100%";
+  }
+
+  closeNav() {
+    this.myRef.current.style.height = "0%";
+  }
+
+  render(){
+    return (
+      <>
+      <div id="myNav" ref={this.myRef}>
+        
+        <div className="flex h-screen justify-center items-center ">
+      
+          <div className="absolute top-0">
+                  <button id ="closebttn" onClick={this.closeNav}>
+                                <GiCrossedSabres></GiCrossedSabres>
+                  </button>
+          </div>
+
+          <div className="text-center">
+                {/* <div className="p-10 bg-gray-500 rounded-lg"> */}
+                  <Link id="menu-links" to="/">About </Link>
+                  <Link id="menu-links" to="/">Experience </Link>
+                  <Link id="menu-links" to="/">Projects </Link>
+                  <Link id="menu-links" to="/">Portfolio </Link>
+                {/* </div> */}
+          </div>
+        </div>
+
+      </div>
+
+      <div className="grid grid-cols-6 gap-4" >
+          <button id ="menubttn" onClick={this.openNav}>
+                      <FaBars></FaBars>
+          </button>
+      </div>
+
+
+    </>
+      
+    );
+  }
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -6,14 +6,14 @@ const Layout = ({ children }) => {
   return (
     <>
       <Seo></Seo>
-      <div className="flex flex-col min-h-screen bg-custom-back-black">
-      <div className="container max-w-screen-md">
-        <Header></Header>
-        {children}
-        <footer className="py-2 text-center text-gray-600 text-xs font-mono">
-          &copy; Sahil Jaganmohan 2021
-        </footer>
-      </div>
+      <div className="flex min-h-screen bg-custom-back-black">
+        <div className="container max-w-screen-md z-0">
+          <Header></Header>
+          {children}
+          <footer className="py-2 text-center text-gray-600 text-xs font-mono">
+            &copy; Sahil Jaganmohan 2021
+          </footer>
+        </div>
       </div>
     </>
   )
